abaf14568ac9f0a25edf7e90be205994f0e191bd
haruki7049 <tontonkirikiri@gmail.com>
2024-03-24 21:33:55 +0900

    chore: Added denops and i3

--- src/home/haruki/home.nix
+++ src/home/haruki/home.nix
@@ -63,6 +63,9 @@ in {
             # plenary, A library for plugin creator
             plenary-nvim
 
+            # denops, A Deno-Vim library for plugin creator
+            denops-vim
+
             # Telescope
             telescope-nvim
             telescope-file-browser-nvim
@@ -173,12 +176,12 @@ in {
             vim.api.nvim_set_keymap("c", "<C-j>", "<Plug>(skkeleton-enable)", {noremap = true});
 
             -- SKKELETON's JISYO
-            --vim.api.nvim_exec([[
-            --  call skkeleton#config({
-            --    \   'globalDictionaries': ['~/.skk/.skk-jisyo'],
-            --    \   'eggLikeNewline': v:true,
-            --    \ })
-            --]], false);
+            vim.api.nvim_exec([[
+              call skkeleton#config({
+                \   'globalDictionaries': ['~/.skk/.skk-jisyo'],
+                \   'eggLikeNewline': v:true,
+                \ })
+            ]], false);
 
             -- LSPCONFIG
             require('lspconfig').rust_analyzer.setup({
@@ -207,6 +210,96 @@ in {
         };
         emacs = {
           enable = true;
+        };
+      };
+
+      xsession = {
+        windowManager.i3 = {
+          enable = true;
+          config = let
+            finishCommand = ''
+              exec "i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -B 'Yes, exit i3' 'i3-msg exit'"
+            '';
+          in rec {
+            modifier = "Mod4";
+            floating.modifier = "Mod4";
+            keybindings = {
+              "${modifier}+Return" = "exec ${pkgs.alacritty}/bin/alacritty";
+              "${modifier}+Shift+q" = "kill";
+              "${modifier}+p" = "exec --no-startup-id ${pkgs.rofi}/bin/rofi -show run";
+
+              "${modifier}+Left" = "focus left";
+              "${modifier}+Right" = "focus right";
+              "${modifier}+Up" = "focus up";
+              "${modifier}+Down" = "focus down";
+
+              "${modifier}+Shift+Left" = "move left";
+              "${modifier}+Shift+Right" = "move right";
+              "${modifier}+Shift+Up" = "move up";
+              "${modifier}+Shift+Down" = "move down";
+
+              "${modifier}+h" = "split h";
+              "${modifier}+v" = "split v";
+              "${modifier}+f" = "fullscreen toggle";
+              "${modifier}+s" = "layout stacking";
+              "${modifier}+w" = "layout tabbed";
+              "${modifier}+e" = "layout toggle split";
+
+              "${modifier}+Shift+space" = "floating toggle";
+              "${modifier}+space" = "focus mode_toggle";
+
+              "${modifier}+a" = "focus parent";
+              "${modifier}+d" = "focus child";
+
+              "${modifier}+Shift+c" = "reload";
+              "${modifier}+Shift+r" = "restart";
+              "${modifier}+Shift+e" = finishCommand;
+
+              "${modifier}+r" = "mode 'resize'";
+
+              "${modifier}+1" = "workspace number 1";
+              "${modifier}+2" = "workspace number 2";
+              "${modifier}+3" = "workspace number 3";
+              "${modifier}+4" = "workspace number 4";
+              "${modifier}+5" = "workspace number 5";
+              "${modifier}+6" = "workspace number 6";
+              "${modifier}+7" = "workspace number 7";
+              "${modifier}+8" = "workspace number 8";
+              "${modifier}+9" = "workspace number 9";
+              "${modifier}+0" = "workspace number 10";
+              "${modifier}+Shift+1" = "move container to workspace number 1";
+              "${modifier}+Shift+2" = "move container to workspace number 2";
+              "${modifier}+Shift+3" = "move container to workspace number 3";
+              "${modifier}+Shift+4" = "move container to workspace number 4";
+              "${modifier}+Shift+5" = "move container to workspace number 5";
+              "${modifier}+Shift+6" = "move container to workspace number 6";
+              "${modifier}+Shift+7" = "move container to workspace number 7";
+              "${modifier}+Shift+8" = "move container to workspace number 8";
+              "${modifier}+Shift+9" = "move container to workspace number 9";
+              "${modifier}+Shift+0" = "move container to workspace number 10";
+            };
+            modes = {
+              resize = {
+                Left = "resize shrink width 10 px or 10 ppt";
+                Right = "resize grow width 10 px or 10 ppt";
+                Up = "resize shrink height 10 px or 10 ppt";
+                Down = "resize grow height 10 px or 10 ppt";
+                Return = "mode 'default'";
+                Escape = "mode 'default'";
+              };
+            };
+            fonts = {
+              names = [ "monospace" ];
+              style = "normal";
+              size = 8.0;
+            };
+            bars = [
+              {
+                position = "bottom";
+                statusCommand = "${pkgs.i3status}/bin/i3status";
+              }
+            ];
+          };
         };
       };
 
