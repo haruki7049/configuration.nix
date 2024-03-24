c88fe84d4e0547415776413a5e5c45ac3c58f849
haruki7049 <tontonkirikiri@gmail.com>
2024-03-24 21:40:54 +0900

    chore: Updated root's config

--- src/home/root/home.nix
+++ src/home/root/home.nix
@@ -1,5 +1,16 @@
 { config, lib, pkgs, ... }:
-{
+let
+  neovimPluginFromGitHub = rev: owner: repo: sha256: pkgs.vimUtils.buildVimPlugin {
+    pname = "${lib.strings.sanitizeDerivationName repo}";
+    version = rev;
+    src = pkgs.fetchFromGitHub {
+      owner = owner;
+      repo = repo;
+      rev = rev;
+      sha256 = sha256;
+    };
+  };
+in {
   home-manager = {
     users.root = {
       home = {
@@ -7,12 +18,179 @@
         homeDirectory = "/root";
       };
 
-      # Enable home-manager
       programs = {
+        # Enable home-manager
         home-manager.enable = true;
+        bash = {
+          enable = true;
+        };
+        git = {
+          enable = true;
+          userName = "haruki7049";
+          userEmail = "tontonkirikiri@gmail.com";
+          extraConfig = {
+            init.defaultBranch = "main";
+          };
+        };
         neovim = {
           enable = true;
           defaultEditor = true;
+          extraPackages = with pkgs; [
+            deno
+            rust-analyzer
+            lua53Packages.lua-lsp
+          ];
+          plugins = with pkgs.vimPlugins; [
+            # zephyr-nvim, A colorscheme for neovim and vim
+            zephyr-nvim
+
+            # plenary, A library for plugin creator
+            plenary-nvim
+
+            # denops, A Deno-Vim library for plugin creator
+            denops-vim
+
+            # Telescope
+            telescope-nvim
+            telescope-file-browser-nvim
+
+            # lspconfig
+            nvim-lspconfig
+
+            # skkeleton, Vim's SKK
+            (neovimPluginFromGitHub "438b9d22d926569db6e6034e0d333edce5f4d4cf" "vim-skk" "skkeleton" "sha256-jXPMDxiyJ3w4cpRgonlXjdmSJHsnkLhG6NeBjYjeKeo=")
+          ];
+          extraLuaConfig = ''
+            -- BASE
+            vim.opt.termguicolors = true
+            vim.opt.number = true
+            vim.opt.syntax = 'on'
+            vim.opt.backup = false
+            vim.opt.swapfile = false
+            vim.scriptencoding = 'utf-8'
+            vim.opt.encoding = 'utf-8'
+            vim.opt.fileencoding = 'utf-8'
+            vim.opt.helplang = "ja", "en"
+            vim.g.loaded_netrw = 1
+            vim.g.loaded_netrwPlugin = 1
+            vim.o.formatoptions = vim.o.formatoptions .. 'jql'
+            vim.opt.laststatus = 0
+            vim.opt.cmdheight = 0
+            vim.api.nvim_create_autocmd({'FileType'}, {
+              pattern = '*',
+              command = 'set formatoptions-=c',
+            })
+
+            -- INDENT
+            vim.api.nvim_create_autocmd({'FileType'}, {
+              pattern = 'lua',
+              command = 'setlocal tabstop=2 shiftwidth=2 et',
+            })
+            vim.api.nvim_create_autocmd({'FileType'}, {
+              pattern = 'rust',
+              command = 'setlocal tabstop=4 shiftwidth=4 et',
+            })
+            vim.api.nvim_create_autocmd({'FileType'}, {
+              pattern = 'html',
+              command = 'setlocal tabstop=4 shiftwidth=4 et',
+            })
+            vim.api.nvim_create_autocmd({'FileType'}, {
+              pattern = 'c',
+              command = 'setlocal tabstop=4 shiftwidth=4 et',
+            })
+            vim.api.nvim_create_autocmd({'FileType'}, {
+              pattern = 'sh',
+              command = 'setlocal tabstop=4 shiftwidth=4 et',
+            })
+            vim.api.nvim_create_autocmd({'FileType'}, {
+              pattern = 'fish',
+              command = 'setlocal tabstop=2 shiftwidth=2 et',
+            })
+            vim.api.nvim_create_autocmd({'FileType'}, {
+              pattern = 'ps1',
+              command = 'setlocal tabstop=4 shiftwidth=4 et',
+            })
+            vim.api.nvim_create_autocmd({'FileType'}, {
+              pattern = 'java',
+              command = 'setlocal tabstop=4 shiftwidth=4 et',
+            })
+            vim.api.nvim_create_autocmd({'FileType'}, {
+              pattern = 'nix',
+              command = 'setlocal tabstop=2 shiftwidth=2 et',
+            })
+            vim.api.nvim_create_autocmd({'FileType'}, {
+              pattern = 'nu',
+              command = 'setlocal tabstop=4 shiftwidth=4 et',
+            })
+            vim.api.nvim_create_autocmd({'FileType'}, {
+              pattern = 'typescriptreact',
+              command = 'setlocal tabstop=2 shiftwidth=2 et',
+            })
+            vim.api.nvim_create_autocmd({'FileType'}, {
+              pattern = 'gleam',
+              command = 'setlocal tabstop=2 shiftwidth=2 et',
+            })
+            vim.api.nvim_create_autocmd({'FileType'}, {
+              pattern = 'lisp',
+              command = 'setlocal tabstop=2 shiftwidth=2 et',
+            })
+            vim.api.nvim_create_autocmd({'FileType'}, {
+              pattern = 'typescript',
+              command = 'setlocal tabstop=2 shiftwidth=2 et',
+            })
+            vim.api.nvim_create_autocmd({'FileType'}, {
+              pattern = 'nginx',
+              command = 'setlocal tabstop=4 shiftwidth=4 et',
+            })
+
+            -- NEOVIDE
+            local font_name = "UDEV Gothic NF:h14"
+            if vim.g.neovide then
+              vim.o.guifont = font_name
+              vim.g.neovide_transparency = 0.98
+              vim.g.transparency = 0.98
+            end
+
+            -- TERMINAL
+            vim.g.sh = "bash"
+
+            -- KEYCONFIG
+            vim.api.nvim_set_keymap("n", "<leader>q", "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown())<cr>", {noremap = true});
+            vim.api.nvim_set_keymap("i", "<C-j>", "<Plug>(skkeleton-enable)", {noremap = true});
+            vim.api.nvim_set_keymap("c", "<C-j>", "<Plug>(skkeleton-enable)", {noremap = true});
+
+            -- SKKELETON's JISYO
+            vim.api.nvim_exec([[
+              call skkeleton#config({
+                \   'globalDictionaries': ['~/.skk/.skk-jisyo'],
+                \   'eggLikeNewline': v:true,
+                \ })
+            ]], false);
+
+            -- LSPCONFIG
+            require('lspconfig').rust_analyzer.setup({
+              settings = {
+                ['rust-analyzer'] = {},
+              },
+            })
+            require('lspconfig').lua_ls.setup({
+              settings = {
+                Lua = {
+                  diagnostics = {
+                    globals = {
+                      'vim',
+                    },
+                  },
+                  telemetry = {
+                    enable = true,
+                  },
+                },
+              },
+            })
+
+            -- COLORSCHEME
+            vim.cmd("colorscheme zephyr")
+          '';
         };
         emacs = {
           enable = true;
