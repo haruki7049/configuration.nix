45be0b76f76d0848a25070be4300326ca8f4c915
haruki7049 <tontonkirikiri@gmail.com>
2024-03-26 10:46:30 +0900

    chore: Added mimelist

--- src/home/haruki/home.nix
+++ src/home/haruki/home.nix
@@ -18,6 +18,21 @@ in {
         homeDirectory = "/home/haruki";
       };
 
+      xdg = {
+        enable = true;
+        mime.enable = true;
+        mimeApps = {
+          enable = true;
+          defaultApplications = {
+            "text/html" = "google-chrome.desktop";
+            "x-scheme-handler/http" = "google-chrome.desktop";
+            "x-scheme-handler/https" = "google-chrome.desktop";
+            "x-scheme-handler/about" = "google-chrome.desktop";
+            "x-scheme-handler/unknown" = "google-chrome.desktop";
+          };
+        };
+      };
+
       programs = {
         # Enable home-manager
         home-manager.enable = true;
