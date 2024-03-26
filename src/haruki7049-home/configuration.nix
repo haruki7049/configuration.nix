ef838ce93f2413b7e4c83dee5c0f298109e4ec05
haruki7049 <tontonkirikiri@gmail.com>
2024-03-26 11:19:30 +0900

    chore: deleted nix-serve

--- src/haruki7049-home/configuration.nix
+++ src/haruki7049-home/configuration.nix
@@ -86,21 +86,6 @@ in {
         PermitRootLogin = "no";
       };
     };
-    nix-serve = {
-      enable = true;
-      port = 5000;
-      bindAddress = "127.0.0.0";
-      openFirewall = true;
-    };
-    nginx = {
-      enable = true;
-      recommendedProxySettings = true;
-      virtualHosts = {
-        "192.168.0.200" = {
-          locations."/".proxyPass = "http://${config.services.nix-serve.bindAddress}:${toString config.services.nix-serve.port}";
-        };
-      };
-    };
   };
 
   system.stateVersion = "23.11";
