6be4d73ad2314c98ecbba5805a1d55cf8ad3855a
haruki7049 <tontonkirikiri@gmail.com>
2024-03-27 21:40:58 +0900

    chore: hoge

--- src/pana-chama/configuration.nix
+++ src/pana-chama/configuration.nix
@@ -57,6 +57,22 @@ in
     style = "adwaita-dark";
   };
 
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
   programs = {
     _1password = {
       enable = true;
