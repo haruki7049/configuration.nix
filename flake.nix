78ece19f8fa76dac65a309344e253e1c6f2aa877
haruki7049 <tontonkirikiri@gmail.com>
2024-04-02 17:43:26 +0900

    feat: Add formatter

--- flake.nix
+++ flake.nix
@@ -3,7 +3,15 @@
 
   inputs.nixos.url = "github:NixOS/nixpkgs/nixos-23.11";
 
-  outputs = { self, nixos }: {
+  outputs = { self, nixos }:
+  let
+    eachSystem = f:
+      nixpkgs.lib.genAttrs (import systems)
+      (system: f nixpkgs.legacyPackages.${system});
+    treefmtEval =
+      eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
+  in
+  {
     # "nixos-rebuild switch --flake .#tuf-chan"
     nixosConfigurations = {
       tuf-chan = nixos.lib.nixosSystem {
@@ -19,5 +27,14 @@
         modules = [ ./src/haruki7049-home/configuration.nix ];
       };
     };
+
+    # Use `nix fmt`
+    formatter =
+      eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+
+    # Use `nix flake check`
+    checks = eachSystem (pkgs: {
+      formatting = treefmtEval.${pkgs.system}.config.build.check self;
+    });
   };
 }
