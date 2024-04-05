e8a81ebb5c951a4e0790f0e350238370b4ba4a3c
haruki7049 <tontonkirikiri@gmail.com>
2024-04-05 19:06:41 +0900

    chore: Add cursor settings

--- src/home/haruki/home.nix
+++ src/home/haruki/home.nix
@@ -35,6 +35,23 @@ in
       home = {
         username = "haruki";
         homeDirectory = "/home/haruki";
+        pointerCursor =
+          let
+            getFrom = url: hash: name: size: {
+              gtk.enable = true;
+              x11.enable = true;
+              name = name;
+              size = size;
+              package = pkgs.runCommand "moveUp" {} ''
+                mkdir -p $out/share/icons
+                ln -s ${pkgs.fetchTarball {
+                  url = url;
+                  hash = hash;
+                }} $out/share/icons/${name}
+              '';
+            };
+          in
+            getFrom "https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.6/Bibata-Modern-Classic.tar.xz" "sha256-jpEuovyLr9HBDsShJo1efRxd21Fxi7HIjXtPJmLQaCU=" "bibata" 24;
       };
 
       xdg = {
