10c8c64744f83f5bf7d04d81e68ad21f73459f0b
haruki7049 <tontonkirikiri@gmail.com>
2024-07-08 13:54:42 +0900

    feat: Init zellij settings

--- src/home/develop/develop.nix
+++ src/home/develop/develop.nix
@@ -22,7 +22,6 @@
     unzip
     gzip
     git
-    zellij
     nixpkgs-fmt
     brave
     google-chrome
@@ -82,6 +81,41 @@
       settings = {
         font.size = 8.0;
         font.normal.family = "UDEV Gothic NF";
+      };
+    };
+    zellij = {
+      enable = true;
+      settings = {
+        theme = "cyber-noir";
+        themes = {
+          cyber-noir = {
+            bg = "#0b0e1a";
+            fg = "#91f3e4";
+            red = "#ff578d";
+            green = "#00ff00";
+            blue = "#3377ff";
+            yellow = "#ffd700";
+            magenta = "#ff00ff";
+            orange = "#ff7f50";
+            cyan = "#00e5e5";
+            black = "#000000";
+            white = "#91f3e4";
+          };
+
+          everforest-dark-medium = {
+            fg = "#d3c6aa";
+            bg = "#2f383e";
+            black = "#4a555b";
+            red = "#d6494d";
+            green = "#a7c080";
+            yellow = "#dbbc7f";
+            blue = "#7fbbb3";
+            magenta = "#d699b6";
+            cyan = "#83c092";
+            white = "#a7c080";
+            orange = "#e69875";
+          };
+        };
       };
     };
   };
