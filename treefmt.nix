d5d6dad99578f7a92f03ae710388af17c041288c
haruki7049 <tontonkirikiri@gmail.com>
2024-07-23 20:19:22 +0900

    style: Init taplo

--- treefmt.nix
+++ treefmt.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   projectRootFile = "flake.nix";
   programs.nixpkgs-fmt.enable = true;
+  programs.taplo.enable = true;
   programs.stylua.enable = true;
   settings.formatter = {
     "stylua".options = [
