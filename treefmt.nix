0c243cfd6312c243a7ece0c8bb6627f9a1c37f52
haruki7049 <tontonkirikiri@gmail.com>
2024-07-23 15:42:02 +0900

    style: Init stylua

--- treefmt.nix
+++ treefmt.nix
@@ -2,4 +2,10 @@
   projectRootFile = "flake.nix";
   programs.nixpkgs-fmt.enable = true;
   programs.stylua.enable = true;
+  settings.formatter = {
+    "stylua".options = [
+      "--indent-type"
+      "Spaces"
+    ];
+  };
 }
