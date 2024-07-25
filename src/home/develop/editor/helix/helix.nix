2546f3b4673ffda6db8ab910d63f404f934d5ef5
haruki7049 <tontonkirikiri@gmail.com>
2024-07-25 14:01:54 +0900

    feat: Init helix editor's language support

--- src/home/develop/editor/helix/helix.nix
+++ src/home/develop/editor/helix/helix.nix
@@ -2,5 +2,17 @@
   programs.helix = {
     enable = true;
     defaultEditor = false;
+    languages = {
+      language = [
+        {
+          name = "rust";
+          auto-format = true;
+        }
+        {
+          name = "zig";
+          auto-format = true;
+        }
+      ];
+    };
   };
 }
