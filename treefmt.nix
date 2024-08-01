3376d6b74a061692d8a596546962443202240e7d
haruki7049 <64677724+haruki7049@users.noreply.github.com>
2024-08-02 08:45:10 +0900

    style: Add actionlint

--- treefmt.nix
+++ treefmt.nix
@@ -3,6 +3,7 @@
   programs.nixpkgs-fmt.enable = true;
   programs.taplo.enable = true;
   programs.stylua.enable = true;
+  programs.actionlint.enable = true;
   settings.formatter = {
     "stylua".options = [
       "--indent-type"
