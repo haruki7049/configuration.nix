68a190a581fb49f380fbd5ae638aa088e22d3478
haruki7049 <tontonkirikiri@gmail.com>
2024-08-05 10:33:44 +0900

    feat: Edit emacs-drv

--- src/home/develop/editor/emacs/emacs-drv/drv.nix
+++ src/home/develop/editor/emacs/emacs-drv/drv.nix
@@ -1,54 +1,9 @@
-{ pname ? "emacs"
-, pkgs
-, version ? "29.4"
-, stdenv ? pkgs.stdenv
-}:
+{ pkgs }:
 
-stdenv.mkDerivation {
-  inherit pname version;
-
-  src = pkgs.fetchFromGitHub {
-    owner = "emacs-mirror";
-    repo = "emacs";
-    rev = "emacs-29.4";
-    hash = "sha256-FCP6ySkN9mAdp2T09n6foS2OciqZXc/54guRZ0B4Z2s=";
-  };
-
-  nativeBuildInputs = with pkgs; [
-    gnumake
-    pkg-config
-  ];
-
-  buildInputs = with pkgs; [
-    autoconf
-    texinfo
-    ncurses
-    gnutls
-    libgccjit
-    tree-sitter
-    alsa-lib
-    gtk3
-    mailutils
-    zlib
-    imagemagick
-    xorg.libXpm
-    giflib
-  ];
-
-  configurePhase = ''
-    $src/autogen.sh
-    ./configure --prefix=$out
-  '';
-
-  configureFlags = [
-    "--with-native-compilation"
-    "--with-tree-sitter"
-    "--with-imagemagick"
-    "--with-ns"
-    "--with-x-toolkit=gtk3"
-    "--with-xwidgets"
-    "--with-x"
-  ];
-
-  meta.platforms = [ "${stdenv.hostPlatform.system}" ];
-}
+pkgs.emacs.overrideAttrs (oldAttrs: {
+  withX = true;
+  withPgtk = true;
+  withGTK3 = true;
+  withXwidgets = true;
+  withAlsaLib = true;
+})
