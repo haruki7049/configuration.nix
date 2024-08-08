b1d97cc1a8bef540fed0458655c3f84e0f242fc7
haruki7049 <tontonkirikiri@gmail.com>
2024-08-08 14:42:09 +0900

    chore: Edit waybar

--- src/home/develop/windowManager/tools/waybar/waybar.nix
+++ src/home/develop/windowManager/tools/waybar/waybar.nix
@@ -9,6 +9,7 @@
         height = 30;
         modules-left = [ "sway/workspaces" "hyprland/workspaces" ];
         modules-center = [ "clock" ];
+        modules-right = [ "tray" ];
 
         clock = {
           format = "{:%H:%M}";
