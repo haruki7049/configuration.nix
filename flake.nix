57405091627e8784ed70d6335fd84345a32f4855
haruki7049 <64677724+haruki7049@users.noreply.github.com>
2024-08-12 18:27:46 +0900

    feat: Using emacs-overlay (#11)

--- flake.nix
+++ flake.nix
@@ -32,28 +32,33 @@
     {
       nixosConfigurations =
         let
-          x86_64-linux-pc = { system ? "x86_64-linux", systemConfiguration, userhome-configs }:
-            let
-              overlays = [
-                (import emacs-overlay)
-              ];
-              pkgs = import nixpkgs {
-                inherit system overlays;
+          x86_64-linux-pc =
+            { system ? "x86_64-linux"
+            , pkgs ? import nixpkgs {
+                inherit system;
                 config.allowUnfree = true;
+              }
+            , systemConfiguration
+            , userhome-configs
+            }:
+            let
+              nixpkgs-overlay-settings = {
+                nixpkgs.overlays = [
+                  emacs-overlay.overlays.emacs
+                ];
               };
             in
             nixpkgs.lib.nixosSystem {
               inherit system;
               modules = [
+                nixpkgs-overlay-settings
                 systemConfiguration
                 home-manager.nixosModules.home-manager
                 {
                   home-manager = {
                     useGlobalPkgs = true;
                     useUserPackages = true;
-                    users = userhome-configs {
-                      inherit pkgs;
-                    };
+                    users = userhome-configs { inherit pkgs; };
                   };
                 }
               ];
