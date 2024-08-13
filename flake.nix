a0e9ec525875560be750dd38233ec6bdedeb3a5b
haruki7049 <64677724+haruki7049@users.noreply.github.com>
2024-08-13 11:59:17 +0900

    rearch: Move x86_64-linux-pc to system-builder.nix (#13)

--- flake.nix
+++ flake.nix
@@ -33,37 +33,7 @@
     {
       nixosConfigurations =
         let
-          x86_64-linux-pc =
-            { system ? "x86_64-linux"
-            , pkgs ? import nixpkgs {
-                inherit system;
-                config.allowUnfree = true;
-              }
-            , systemConfiguration
-            , userhome-configs
-            }:
-            let
-              nixpkgs-overlay-settings = {
-                nixpkgs.overlays = [
-                  emacs-overlay.overlays.emacs
-                ];
-              };
-            in
-            nixpkgs.lib.nixosSystem {
-              inherit system;
-              modules = [
-                nixpkgs-overlay-settings
-                systemConfiguration
-                home-manager.nixosModules.home-manager
-                {
-                  home-manager = {
-                    useGlobalPkgs = true;
-                    useUserPackages = true;
-                    users = userhome-configs { inherit pkgs; };
-                  };
-                }
-              ];
-            };
+          inherit (import ./system-builder.nix { inherit nixpkgs emacs-overlay home-manager; }) x86_64-linux-pc;
         in
         {
           tuf-chan = x86_64-linux-pc {
