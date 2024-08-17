7f6e918da078f598deb3473d72b029de99353e5e
haruki7049 <tontonkirikiri@gmail.com>
2024-08-18 00:00:50 +0900

    feat(mimeapps.list): Edit default browser from google-chrome to firefox

--- src/home/users/haruki.nix
+++ src/home/users/haruki.nix
@@ -30,11 +30,35 @@ in
     mimeApps = {
       enable = true;
       defaultApplications = {
-        "text/html" = "google-chrome.desktop";
-        "x-scheme-handler/http" = "google-chrome.desktop";
-        "x-scheme-handler/https" = "google-chrome.desktop";
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
         "x-scheme-handler/about" = "google-chrome.desktop";
         "x-scheme-handler/unknown" = "google-chrome.desktop";
+        "text/html" = "firefox.desktop";
+        "application/x-extension-htm" = "firefox.desktop";
+        "application/x-extension-html" = "firefox.desktop";
+        "application/x-extension-shtml" = "firefox.desktop";
+        "application/xhtml+xml" = "firefox.desktop";
+        "application/x-extension-xhtml" = "firefox.desktop";
+        "application/x-extension-xht" = "firefox.desktop";
+      };
+
+      associations = {
+        added = {
+          "x-scheme-handler/http" = "firefox.desktop";
+          "x-scheme-handler/https" = "firefox.desktop";
+          "x-scheme-handler/about" = "google-chrome.desktop";
+          "x-scheme-handler/unknown" = "google-chrome.desktop";
+          "text/html" = "firefox.desktop";
+          "application/x-extension-htm" = "firefox.desktop";
+          "application/x-extension-html" = "firefox.desktop";
+          "application/x-extension-shtml" = "firefox.desktop";
+          "application/xhtml+xml" = "firefox.desktop";
+          "application/x-extension-xhtml" = "firefox.desktop";
+          "application/x-extension-xht" = "firefox.desktop";
+        };
+
+        removed = { };
       };
     };
   };
