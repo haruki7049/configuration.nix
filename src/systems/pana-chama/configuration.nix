3c6e9484c436359b7606faef1f4537beeac93df5
haruki7049 <tontonkirikiri@gmail.com>
2024-08-22 14:23:35 +0900

    chore(pana-chama): Delete 1password

--- src/systems/pana-chama/configuration.nix
+++ src/systems/pana-chama/configuration.nix
@@ -70,11 +70,6 @@
   };
 
   programs = {
-    _1password = { enable = true; };
-    _1password-gui = {
-      enable = true;
-      polkitPolicyOwners = [ "haruki" ];
-    };
     steam = {
       enable = true;
       remotePlay.openFirewall = true;
