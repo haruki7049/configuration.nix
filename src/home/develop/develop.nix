a748e67255c7cfa81e25778fb7cbd0d9cfc8dfa0
haruki7049 <tontonkirikiri@gmail.com>
2024-09-22 10:01:14 +0900

    chore(develop): Edit

--- src/home/develop/develop.nix
+++ src/home/develop/develop.nix
@@ -86,6 +86,32 @@
       "https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.6/Bibata-Modern-Classic.tar.xz"
       "sha256-jpEuovyLr9HBDsShJo1efRxd21Fxi7HIjXtPJmLQaCU=" "bibata" 24;
 
+  xresources = {
+    properties = {
+      # Dracula palette
+      "*.foreground" = "#F8F8F2";
+      "*.background" = "#282A36";
+      "*.color0" = "#000000";
+      "*.color8" = "#4D4D4D";
+      "*.color1" = "#FF5555";
+      "*.color9" = "#FF6E67";
+      "*.color2" = "#50FA7B";
+      "*.color10" = "#5AF78E";
+      "*.color3" =  "#F1FA8C";
+      "*.color11" =  "#F4F99D";
+      "*.color4" =  "#BD93F9";
+      "*.color12" =  "#CAA9FA";
+      "*.color5" =  "#FF79C6";
+      "*.color13" =  "#FF92D0";
+      "*.color6" =  "#8BE9FD";
+      "*.color14" =  "#9AEDFE";
+      "*.color7" =  "#BFBFBF";
+      "*.color15" =  "#E6E6E6";
+    };
+    extraConfig = ''
+    '';
+  };
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   nixpkgs = {
