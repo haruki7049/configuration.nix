e03ce323bd67436f34cb175540c3ab9c855329a1
haruki7049 <tontonkirikiri@gmail.com>
2025-05-02 20:06:17 +0900

    style: nix fmt

--- src/home/darwin/develop/editor/neovim/init.lua
+++ src/home/darwin/develop/editor/neovim/init.lua
@@ -15,91 +15,91 @@ vim.o.formatoptions = vim.o.formatoptions .. "jql"
 vim.opt.laststatus = 0
 vim.opt.cmdheight = 0
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "*",
-  command = "set formatoptions-=c",
+    pattern = "*",
+    command = "set formatoptions-=c",
 })
 
 -- INDENT
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "lua",
-  command = "setlocal tabstop=2 shiftwidth=2 et",
+    pattern = "lua",
+    command = "setlocal tabstop=2 shiftwidth=2 et",
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "rust",
-  command = "setlocal tabstop=4 shiftwidth=4 et",
+    pattern = "rust",
+    command = "setlocal tabstop=4 shiftwidth=4 et",
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "html",
-  command = "setlocal tabstop=4 shiftwidth=4 et",
+    pattern = "html",
+    command = "setlocal tabstop=4 shiftwidth=4 et",
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "c",
-  command = "setlocal tabstop=4 shiftwidth=4 et",
+    pattern = "c",
+    command = "setlocal tabstop=4 shiftwidth=4 et",
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "sh",
-  command = "setlocal tabstop=4 shiftwidth=4 et",
+    pattern = "sh",
+    command = "setlocal tabstop=4 shiftwidth=4 et",
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "fish",
-  command = "setlocal tabstop=2 shiftwidth=2 et",
+    pattern = "fish",
+    command = "setlocal tabstop=2 shiftwidth=2 et",
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "ruby",
-  command = "setlocal tabstop=2 shiftwidth=2 et",
+    pattern = "ruby",
+    command = "setlocal tabstop=2 shiftwidth=2 et",
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "ps1",
-  command = "setlocal tabstop=4 shiftwidth=4 et",
+    pattern = "ps1",
+    command = "setlocal tabstop=4 shiftwidth=4 et",
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "java",
-  command = "setlocal tabstop=4 shiftwidth=4 et",
+    pattern = "java",
+    command = "setlocal tabstop=4 shiftwidth=4 et",
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "nix",
-  command = "setlocal tabstop=2 shiftwidth=2 et",
+    pattern = "nix",
+    command = "setlocal tabstop=2 shiftwidth=2 et",
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "nu",
-  command = "setlocal tabstop=4 shiftwidth=4 et",
+    pattern = "nu",
+    command = "setlocal tabstop=4 shiftwidth=4 et",
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "typescriptreact",
-  command = "setlocal tabstop=2 shiftwidth=2 et",
+    pattern = "typescriptreact",
+    command = "setlocal tabstop=2 shiftwidth=2 et",
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "gleam",
-  command = "setlocal tabstop=2 shiftwidth=2 et",
+    pattern = "gleam",
+    command = "setlocal tabstop=2 shiftwidth=2 et",
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "lisp",
-  command = "setlocal tabstop=2 shiftwidth=2 et",
+    pattern = "lisp",
+    command = "setlocal tabstop=2 shiftwidth=2 et",
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "typescript",
-  command = "setlocal tabstop=2 shiftwidth=2 et",
+    pattern = "typescript",
+    command = "setlocal tabstop=2 shiftwidth=2 et",
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "nginx",
-  command = "setlocal tabstop=4 shiftwidth=4 et",
+    pattern = "nginx",
+    command = "setlocal tabstop=4 shiftwidth=4 et",
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "json",
-  command = "setlocal tabstop=2 shiftwidth=2 et",
+    pattern = "json",
+    command = "setlocal tabstop=2 shiftwidth=2 et",
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "*",
-  command = 'if &l:omnifunc == "" | setlocal omnifunc=syntaxcomplete#Complete | endif',
+    pattern = "*",
+    command = 'if &l:omnifunc == "" | setlocal omnifunc=syntaxcomplete#Complete | endif',
 })
 
 -- NEOVIDE
 local font_name = "UDEV Gothic NF:h14"
 if vim.g.neovide then
-  vim.o.guifont = font_name
-  vim.g.neovide_transparency = 0.98
-  vim.g.transparency = 0.98
+    vim.o.guifont = font_name
+    vim.g.neovide_transparency = 0.98
+    vim.g.transparency = 0.98
 end
 
 -- TERMINAL
@@ -107,132 +107,132 @@ vim.g.sh = "bash"
 
 -- KEYCONFIG
 vim.keymap.set(
-  "n",
-  "<leader>q",
-  "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown())<cr>"
+    "n",
+    "<leader>q",
+    "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown())<cr>"
 )
 vim.keymap.set("i", "<C-j>", "<Plug>(skkeleton-enable)")
 vim.keymap.set("c", "<C-j>", "<Plug>(skkeleton-enable)")
 vim.keymap.set("n", "<Leader><Leader>", require("Comment.api").toggle.linewise.current)
 vim.keymap.set("x", "<Leader><Leader>", function()
-  vim.api.nvim_feedkeys("<Esc>", "nx", false)
-  require("Comment.api").toggle.linewise(vim.fn.visualmode())
+    vim.api.nvim_feedkeys("<Esc>", "nx", false)
+    require("Comment.api").toggle.linewise(vim.fn.visualmode())
 end)
 
 -- Comment out, by Comment.nvim
 require("Comment").setup({
-  padding = true,
-  sticky = true,
-  ignore = nil,
+    padding = true,
+    sticky = true,
+    ignore = nil,
 
-  mappings = {
-    basic = false,
-    extra = false,
-  },
+    mappings = {
+        basic = false,
+        extra = false,
+    },
 
-  pre_hook = nil,
-  post_hook = nil,
+    pre_hook = nil,
+    post_hook = nil,
 })
 
 -- LSPCONFIG
 require("lspconfig").rust_analyzer.setup({
-  settings = {
-    ["rust-analyzer"] = {},
-  },
+    settings = {
+        ["rust-analyzer"] = {},
+    },
 })
 require("lspconfig").denols.setup({
-  root_dir = require("lspconfig").util.root_pattern("deno.json"),
-  settings = {
-    ["denols"] = {
-      cmd = {
-        "deno",
-        "lsp",
-      },
-      filetypes = {
-        "javascript",
-        "javascriptreact",
-        "javascript.jsx",
-        "typescript",
-        "typescriptreact",
-        "typescript.tsx",
-      },
-      settings = {
-        deno = {
-          enable = true,
-          suggest = {
-            imports = {
-              hosts = {
-                ["https://deno.land"] = true,
-                ["https://jsr.io"] = true,
-              },
+    root_dir = require("lspconfig").util.root_pattern("deno.json"),
+    settings = {
+        ["denols"] = {
+            cmd = {
+                "deno",
+                "lsp",
             },
-          },
+            filetypes = {
+                "javascript",
+                "javascriptreact",
+                "javascript.jsx",
+                "typescript",
+                "typescriptreact",
+                "typescript.tsx",
+            },
+            settings = {
+                deno = {
+                    enable = true,
+                    suggest = {
+                        imports = {
+                            hosts = {
+                                ["https://deno.land"] = true,
+                                ["https://jsr.io"] = true,
+                            },
+                        },
+                    },
+                },
+            },
         },
-      },
     },
-  },
 })
 require("lspconfig").ts_ls.setup({
-  root_dir = require("lspconfig").util.root_pattern("package.json"),
+    root_dir = require("lspconfig").util.root_pattern("package.json"),
 })
 require("lspconfig").ruff.setup({
-  settings = {
-    ["ruff"] = {
-      cmd = {
-        "ruff",
-        "server",
-        "--preview",
-      },
-      filetypes = {
-        "python",
-      },
-      single_file_support = true,
+    settings = {
+        ["ruff"] = {
+            cmd = {
+                "ruff",
+                "server",
+                "--preview",
+            },
+            filetypes = {
+                "python",
+            },
+            single_file_support = true,
+        },
     },
-  },
 })
 require("lspconfig").rubocop.setup({})
 require("lspconfig").lua_ls.setup({
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = {
-          "vim",
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = {
+                    "vim",
+                },
+            },
+            telemetry = {
+                enable = true,
+            },
         },
-      },
-      telemetry = {
-        enable = true,
-      },
     },
-  },
 })
 require("lspconfig").nil_ls.setup({
-  autostart = true,
-  settings = {
-    ["nil"] = {
-      testSettings = 42,
-      formatting = {
-        command = { "nixpkgs-fmt" },
-      },
+    autostart = true,
+    settings = {
+        ["nil"] = {
+            testSettings = 42,
+            formatting = {
+                command = { "nixpkgs-fmt" },
+            },
+        },
     },
-  },
 })
 require("lspconfig").gopls.setup({
-  settings = {
-    gopls = {
-      analyses = {
-        unusedparams = true,
-      },
-      gofumpt = true,
+    settings = {
+        gopls = {
+            analyses = {
+                unusedparams = true,
+            },
+            gofumpt = true,
+        },
     },
-  },
 })
 require("lspconfig").gleam.setup({
-  autostart = true,
+    autostart = true,
 })
 require("lspconfig").zls.setup({
-  settings = {
-    diagnostics = false,
-  },
+    settings = {
+        diagnostics = false,
+    },
 })
 
 -- COLORSCHEME
