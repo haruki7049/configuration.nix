c7df584e3568cbe45dfd81c96388606f141c99d0
haruki7049 <tontonkirikiri@gmail.com>
2025-05-06 23:09:29 +0900

    config(linux-darwin-neovim): Update default.nix

--- src/home/darwin/develop/editor/neovim/default.nix
+++ src/home/darwin/develop/editor/neovim/default.nix
@@ -6,7 +6,12 @@
 
 let
   neovimPluginFromGitHub =
-    rev: owner: repo: sha256:
+    {
+      owner,
+      repo,
+      rev,
+      sha256,
+    }:
     pkgs.vimUtils.buildVimPlugin {
       pname = "${lib.strings.sanitizeDerivationName repo}";
       version = rev;
@@ -60,14 +65,28 @@ in
       ])
       ++ [
         # skkeleton, Vim's SKK
-        (neovimPluginFromGitHub "cf385775279c0c7eed3fbebfac1022f1f05ea292" "vim-skk" "skkeleton"
-          "sha256-DA/k2KxGqxYtyJcnV1g2lLbMtNKBXpPGje5WeYYnbtQ="
-        )
+        (neovimPluginFromGitHub {
+          owner = "vim-skk";
+          repo = "skkeleton";
+          rev = "cf385775279c0c7eed3fbebfac1022f1f05ea292";
+          sha256 = "sha256-DA/k2KxGqxYtyJcnV1g2lLbMtNKBXpPGje5WeYYnbtQ=";
+        })
 
-        # Comment out
-        (neovimPluginFromGitHub "0236521ea582747b58869cb72f70ccfa967d2e89" "numToStr" "Comment.nvim"
-          "sha256-+dF1ZombrlO6nQggufSb0igXW5zwU++o0W/5ZA07cdc="
-        )
+        # janet-vim
+        (neovimPluginFromGitHub {
+          owner = "janet-lang";
+          repo = "janet.vim";
+          rev = "67075b190a44310df356137e35cc1949782b20e0";
+          sha256 = "sha256-TmbInorBSlKSK/D59izThad7PSfH2JmJdAnbOMhixHA=";
+        })
+
+        # pest.vim
+        (neovimPluginFromGitHub {
+          owner = "pest-parser";
+          repo = "pest.vim";
+          rev = "7cfcb43f824e74d13dfe631359fff2ec23836a77";
+          sha256 = "sha256-EQcMSsKWtQvr0eQ6Hn0TtDA5Nc7VV0g2bnbx7i2B7u4=";
+        })
       ];
     extraLuaConfig = lib.strings.concatStrings [
       (builtins.readFile ./init.lua)
