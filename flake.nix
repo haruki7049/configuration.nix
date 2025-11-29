2f55548e070804fab7fc174599bea89f969d59ef
haruki7049 <tontonkirikiri@gmail.com>
2025-11-29 23:35:10 +0900

    fix(flake.nix): Upgrade home-manager to master branch

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     treefmt-nix = {
