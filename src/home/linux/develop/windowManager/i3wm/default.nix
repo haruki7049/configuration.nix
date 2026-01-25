f2de7a9730acc261f9c5be718a9d28b0e397a0c8
haruki7049 <tontonkirikiri@gmail.com>
2026-01-25 21:51:56 +0900

    Update i3wm settings

--- src/home/linux/develop/windowManager/i3wm/default.nix
+++ src/home/linux/develop/windowManager/i3wm/default.nix
@@ -1,105 +1,91 @@
 {
   lib,
   pkgs,
+  config,
   ...
 }:
+
+let
+  cfg = config.xsession.windowManager.i3;
+in
 
 {
   xsession.windowManager.i3 = {
     enable = true;
-    config =
-      let
-        finishCommand = ''
-          exec "i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -B 'Yes, exit i3' 'i3-msg exit'"
-        '';
-      in
-      rec {
-        startup = [
-          {
-            command = lib.strings.concatStringsSep " " [
-              (lib.getExe pkgs.feh)
-              "--bg-center"
-              (pkgs.callPackage ../wallpapers/use-nix_nixos.nix { }).outPath
-            ];
-            always = true;
-            notification = false;
-          }
-        ];
+    config = {
+      modifier = "Mod4";
+      floating.modifier = "Mod4";
+      terminal = "alacritty";
+      keybindings = {
+        "${cfg.config.modifier}+Return" = "exec ${cfg.config.terminal}";
+        "${cfg.config.modifier}+Shift+q" = "kill";
+        "${cfg.config.modifier}+p" = "exec --no-startup-id ${pkgs.rofi}/bin/rofi -show drun";
 
-        modifier = "Mod4";
-        floating.modifier = "Mod4";
-        keybindings = {
-          "${modifier}+Return" = "exec ${pkgs.alacritty}/bin/alacritty";
-          "${modifier}+Shift+q" = "kill";
-          "${modifier}+p" = "exec --no-startup-id ${pkgs.rofi}/bin/rofi -show drun";
+        "${cfg.config.modifier}+Left" = "focus left";
+        "${cfg.config.modifier}+Right" = "focus right";
+        "${cfg.config.modifier}+Up" = "focus up";
+        "${cfg.config.modifier}+Down" = "focus down";
 
-          "${modifier}+Left" = "focus left";
-          "${modifier}+Right" = "focus right";
-          "${modifier}+Up" = "focus up";
-          "${modifier}+Down" = "focus down";
+        "${cfg.config.modifier}+Shift+Left" = "move left";
+        "${cfg.config.modifier}+Shift+Right" = "move right";
+        "${cfg.config.modifier}+Shift+Up" = "move up";
+        "${cfg.config.modifier}+Shift+Down" = "move down";
 
-          "${modifier}+Shift+Left" = "move left";
-          "${modifier}+Shift+Right" = "move right";
-          "${modifier}+Shift+Up" = "move up";
-          "${modifier}+Shift+Down" = "move down";
+        "${cfg.config.modifier}+h" = "split h";
+        "${cfg.config.modifier}+v" = "split v";
+        "${cfg.config.modifier}+f" = "fullscreen toggle";
+        "${cfg.config.modifier}+s" = "layout stacking";
+        "${cfg.config.modifier}+w" = "layout tabbed";
+        "${cfg.config.modifier}+e" = "layout toggle split";
 
-          "${modifier}+h" = "split h";
-          "${modifier}+v" = "split v";
-          "${modifier}+f" = "fullscreen toggle";
-          "${modifier}+s" = "layout stacking";
-          "${modifier}+w" = "layout tabbed";
-          "${modifier}+e" = "layout toggle split";
+        "${cfg.config.modifier}+Shift+space" = "floating toggle";
+        "${cfg.config.modifier}+space" = "focus mode_toggle";
 
-          "${modifier}+Shift+space" = "floating toggle";
-          "${modifier}+space" = "focus mode_toggle";
+        "${cfg.config.modifier}+a" = "focus parent";
+        "${cfg.config.modifier}+d" = "focus child";
 
-          "${modifier}+a" = "focus parent";
-          "${modifier}+d" = "focus child";
+        "${cfg.config.modifier}+Shift+c" = "reload";
+        "${cfg.config.modifier}+Shift+r" = "restart";
+        "${cfg.config.modifier}+Shift+e" =
+          "exec i3-nagbar -t warning -m 'Do you want to exit i3?' -b 'Yes' 'i3-msg exit'";
 
-          "${modifier}+Shift+c" = "reload";
-          "${modifier}+Shift+r" = "restart";
-          "${modifier}+Shift+e" = finishCommand;
+        "${cfg.config.modifier}+r" = "mode 'resize'";
+        "${cfg.config.modifier}+l" = "exec i3lock";
 
-          "${modifier}+r" = "mode 'resize'";
-
-          "${modifier}+j" = "exec $GUI_EDITOR";
-          "${modifier}+l" = "exec i3lock";
-          "${modifier}+b" = "exec $BROWSER";
-
-          "${modifier}+1" = "workspace number 1";
-          "${modifier}+2" = "workspace number 2";
-          "${modifier}+3" = "workspace number 3";
-          "${modifier}+4" = "workspace number 4";
-          "${modifier}+5" = "workspace number 5";
-          "${modifier}+6" = "workspace number 6";
-          "${modifier}+7" = "workspace number 7";
-          "${modifier}+8" = "workspace number 8";
-          "${modifier}+9" = "workspace number 9";
-          "${modifier}+Shift+1" = "move container to workspace number 1";
-          "${modifier}+Shift+2" = "move container to workspace number 2";
-          "${modifier}+Shift+3" = "move container to workspace number 3";
-          "${modifier}+Shift+4" = "move container to workspace number 4";
-          "${modifier}+Shift+5" = "move container to workspace number 5";
-          "${modifier}+Shift+6" = "move container to workspace number 6";
-          "${modifier}+Shift+7" = "move container to workspace number 7";
-          "${modifier}+Shift+8" = "move container to workspace number 8";
-          "${modifier}+Shift+9" = "move container to workspace number 9";
-        };
-        modes = {
-          resize = {
-            Left = "resize shrink width 10 px or 10 ppt";
-            Right = "resize grow width 10 px or 10 ppt";
-            Up = "resize shrink height 10 px or 10 ppt";
-            Down = "resize grow height 10 px or 10 ppt";
-            Return = "mode 'default'";
-            Escape = "mode 'default'";
-          };
-        };
-        fonts = {
-          names = [ "UDEV Gothic NF" ];
-          style = "normal";
-          size = 8.0;
+        "${cfg.config.modifier}+1" = "workspace number 1";
+        "${cfg.config.modifier}+2" = "workspace number 2";
+        "${cfg.config.modifier}+3" = "workspace number 3";
+        "${cfg.config.modifier}+4" = "workspace number 4";
+        "${cfg.config.modifier}+5" = "workspace number 5";
+        "${cfg.config.modifier}+6" = "workspace number 6";
+        "${cfg.config.modifier}+7" = "workspace number 7";
+        "${cfg.config.modifier}+8" = "workspace number 8";
+        "${cfg.config.modifier}+9" = "workspace number 9";
+        "${cfg.config.modifier}+Shift+1" = "move container to workspace number 1";
+        "${cfg.config.modifier}+Shift+2" = "move container to workspace number 2";
+        "${cfg.config.modifier}+Shift+3" = "move container to workspace number 3";
+        "${cfg.config.modifier}+Shift+4" = "move container to workspace number 4";
+        "${cfg.config.modifier}+Shift+5" = "move container to workspace number 5";
+        "${cfg.config.modifier}+Shift+6" = "move container to workspace number 6";
+        "${cfg.config.modifier}+Shift+7" = "move container to workspace number 7";
+        "${cfg.config.modifier}+Shift+8" = "move container to workspace number 8";
+        "${cfg.config.modifier}+Shift+9" = "move container to workspace number 9";
+      };
+      modes = {
+        resize = {
+          Left = "resize shrink width 10 px or 10 ppt";
+          Right = "resize grow width 10 px or 10 ppt";
+          Up = "resize shrink height 10 px or 10 ppt";
+          Down = "resize grow height 10 px or 10 ppt";
+          Return = "mode 'default'";
+          Escape = "mode 'default'";
         };
       };
+      fonts = {
+        names = [ "UDEV Gothic NF" ];
+        style = "normal";
+        size = 8.0;
+      };
+    };
   };
 }
