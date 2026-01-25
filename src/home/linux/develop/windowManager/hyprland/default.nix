87b5d8cc337db5c230e7be025dd44f59e5d6fa34
haruki7049 <tontonkirikiri@gmail.com>
2026-01-25 22:23:46 +0900

    Update hyprland/default.nix

--- src/home/linux/develop/windowManager/hyprland/default.nix
+++ src/home/linux/develop/windowManager/hyprland/default.nix
@@ -8,16 +8,6 @@
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
-      env = [
-        "XCURSOR_SIZE, 24"
-        "HYPRCURSOR_SIZE, 24"
-      ];
-
-      monitor = [
-        "eDP-1, preferred, 0x0, 1"
-        "HDMI-A-1, preferred, 0x0, 1"
-        "DP-1, preferred, 1920x0, 1"
-      ];
       dwindle = {
         preserve_split = true;
       };
@@ -42,18 +32,7 @@
           vibrancy = 0.1696;
         };
       };
-      animations = {
-        enabled = true;
-        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
-        animation = [
-          "windows, 1, 7, myBezier"
-          "windowsOut, 1, 7, default, popin 80%"
-          "border, 1, 10, default"
-          "borderangle, 1, 8, default"
-          "fade, 1, 7, default"
-          "workspaces, 1, 6, default"
-        ];
-      };
+      animations.enabled = false;
       misc = {
         force_default_wallpaper = -1;
         disable_hyprland_logo = false;
