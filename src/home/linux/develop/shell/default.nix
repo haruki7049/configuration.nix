6aaf0c14240f0de1e49f14fd05f16fe6de5d23f2
haruki7049 <tontonkirikiri@gmail.com>
2026-04-25 17:31:19 +0900

    Update shell/default.nix

--- src/home/linux/develop/shell/default.nix
+++ src/home/linux/develop/shell/default.nix
@@ -11,6 +11,10 @@
   programs = {
     bash.enable = true;
     fish.enable = true;
+
     zsh.enable = true;
+    zsh.initContent = ''
+      ${pkgs.any-nix-shell}/bin/any-nix-shell zsh --info-right | source /dev/stdin
+    '';
   };
 }
